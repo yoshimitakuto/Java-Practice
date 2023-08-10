90d87360b31f94d85a83e20ffa56aef6d69e7832
Yoshimi Takuto <yocchantaku0127@gmail.com>
2023-08-10 17:50:24 +0900

    first commit

---
+++ greeting.java
@@ -0,0 +1,22 @@
+class Greeting{
+    public static void main(String args[]){
+        System.out.println("good morning");
+        System.out.println("good afternoon");
+        System.out.println("good morning");
+
+        int var = 1;
+        int age = 20;
+
+        System.out.println(var);
+
+        if (age >= 20){
+            System.out.println("20以上です");
+        } else if (age == 20) {
+            System.out.println("20未満から1の間です");
+        } else {
+            System.out.println("それ以外です");
+        }
+
+
+    }
+}
